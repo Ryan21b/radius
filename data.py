644f2356158ed3dde610b95739e8c0e17f559495
Ryan21b <barronryan65@gmail.com>
2020-03-10 10:15:17 +0200

    new

---
+++ data.py
@@ -0,0 +1,26 @@
+x = "Coding academy"#declaring the variable of x
+print(type(x))#display data type of x
+print(x[2])
+
+number1 = 5
+number2 = 5.8
+print(type(number1))#declare data type int
+print (type(number2))#declare data type float
+
+number3 = 5j
+print(type(number3))#declare data type complex
+
+mylist = [23, 82, 42, 12]
+
+print(type(mylist))#declaring data types
+
+mylist.insert(2 ,34,)#testing .insert
+print(mylist)
+
+mylist.reverse()#reverses the data list
+print(mylist)
+mylist.remove(82)#removes inserted value
+print(mylist)
+mylist.sort()
+print(mylist)
+
